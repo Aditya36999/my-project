e7cb92529f2ead87e273615783ea71c207b08eeb
Aditya36999 <23bai70682@cuchd.in>
2025-08-25 12:34:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var readDocs = document.querySelector(".read-instructions");
-    readDocs.style.display = "block";
+var categorySelect = document.getElementById("category");
+var productList = document.getElementById("product-list");
+var products = productList.getElementsByTagName("li");
+
+categorySelect.addEventListener("change", function() {
+  var selectedCategory = categorySelect.value;
+
+  for (var i = 0; i < products.length; i++) {
+    var productCategory = products[i].getAttribute("data-category");
+
+    if (selectedCategory === "All" || selectedCategory === productCategory) {
+      products[i].style.display = "block";
+    } else {
+      products[i].style.display = "none";
+    }
+  }
 });
